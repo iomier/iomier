1040720aece8c9dec2268f543a89e25dfc676c45
Iman Omidi <iomier@users.noreply.github.com>
2022-08-18 01:40:47 +0430

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import solidJs from "@astrojs/solid-js";
-
+import netlify from '@astrojs/netlify/functions';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), solidJs(), tailwind()]
+  integrations: [mdx(), sitemap(), solidJs(), tailwind()],
+  output: 'server',
+  adapter: netlify(),
 });
